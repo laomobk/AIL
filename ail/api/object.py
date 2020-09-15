788d09d9b3360e2e2a94403eb80fe38ad9257cd6
LaomoBK <laomobk@163.com>
2020-09-16 00:44:52 +0800

    refactor: 准备构建文档

--- ail/api/object.py
+++ ail/api/object.py
@@ -1,3 +1,13 @@
+"""
+AIL Python API -- object
+
+The collection of AIL Objs API.
+You can almost do anything through these API.
+But please use them normatively.
+
+AIL is fragile.
+"""
+
 
 from ..objects.array import (
     ARRAY_TYPE, 
@@ -25,21 +35,75 @@ from ..objects.struct import (
     convert_to_pyobj as _convert_to_pyobj,
 )
 
-from ..objects.type import _TYPE_TYPE
+from ..objects.type import TYPE_TYPE
 from ..objects.wrapper import WRAPPER_TYPE
 
 from ..core.aobjects import (
     convert_to_ail_object as _convert_to_ail_object,
     unpack_ailobj as _unpack_ailobj,
+    AILObject,
+    AILObjectType
 )
 
 
 __all__ = [
     'ARRAY_TYPE', 'BOOL_TYPE', 'FLOAT_TYPE', 'FUNCTION_TYPE', 
     'INTEGER_TYPE', 'STRING_TYPE', 'STRUCT_TYPE', 'WRAPPER_TYPE',
-    'null, convert_to_ail_object', 'unpack_ailobj'
+    'null, convert_to_ail_object', 'unpack_ailobj',
+    'AILObject', 'AILObjectType',
 ]
 
 
-def convert_to_ail_object():
-    return _convert_to_ail_object
+def convert_to_ail_object(pyobj: object) -> AILObject:
+    """
+    Convert a python object to an AIL object.
+
+    normally, only 'constant' type can be converted to target type AIL object.
+    if AIL cannot convert this object, will return `Python Object Wrapper`
+
+    * supporting types:
+        int
+        float
+        str
+        bool
+        list
+        python function  (only python function, not method or built-in function, etc.)
+
+
+    >>> from ail.api.object import convert_to_ail_object
+    >>>
+    >>> aint = convert_to_ail_object(726)
+    >>> aint
+    < 726 >
+    >>>
+    >>> aint['__class__']
+    <AIL Type '<AIL integer type>'>
+
+    >>> aarray = convert_to_ail_object(['Nezha', 'Aobing'])
+    >>>
+    >>> aarray
+    {'Nezha', 'Aobing'}
+    >>> aarray['__class__']
+    <AIL Type '<AIL array type>'>
+
+    :param pyobj: python object.
+    :return: AIL object
+    """
+    return _convert_to_ail_object(pyobj)
+
+
+def get_type(ailobj: AILObject) -> AILObjectType:
+    """
+    Get the AILObjectType object from an AIL object
+
+    >>> from ail.api.object import get_type, convert_to_ail_object
+    >>>
+    >>> get_type(convert_to_ail_object(726))
+    <AIL Type '<AIL integer type>'>
+
+    :param ailobj: an AIL object WHICH HAS TYPE
+    :return: the AILObjectType object of that  AIL object
+    """
+    return ailobj['__class__'] if isinstance(ailobj, AILObject) else None
+
+
---
+++ doc/zh_cn/_build/html/_sources/index.rst.txt
@@ -0,0 +1,20 @@
+.. AIL-doc documentation master file, created by
+   sphinx-quickstart on Wed Sep 16 00:24:58 2020.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+Welcome to AIL-doc's documentation!
+===================================
+
+.. toctree::
+   :maxdepth: 2
+   :caption: Contents:
+
+
+
+Indices and tables
+==================
+
+* :ref:`genindex`
+* :ref:`modindex`
+* :ref:`search`
