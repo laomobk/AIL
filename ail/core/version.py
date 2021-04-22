33da117fde78b8bb6036089dd60cca64c882048d
LaomoBK <laomobk@163.com>
2021-04-23 00:20:46 +0800

    alpha 3

--- ail/core/version.py
+++ ail/core/version.py
@@ -1,7 +1,7 @@
 
 AIL_MAIN_VERSION = 1
 AIL_SUB_VERSION = [2]
-AIL_VERSION_STATE = 'alpha 2'
+AIL_VERSION_STATE = 'alpha 3'
 AIL_VERSION = '%s.%s %s' % (AIL_MAIN_VERSION,
                             '.'.join([str(v) for v in AIL_SUB_VERSION]),
                             AIL_VERSION_STATE)
