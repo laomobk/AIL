84a34843519e4c783e5f5622ff67646171effed2
Laomo <laomobk@163.com>
2022-03-03 12:58:37 +0800

    **alpha 2**

--- ail/core/version.py
+++ ail/core/version.py
@@ -4,7 +4,7 @@ from .._config import CORE_PATH
 
 AIL_MAIN_VERSION = 2
 AIL_SUB_VERSION = [2]
-AIL_VERSION_STATE = 'alpha 1'
+AIL_VERSION_STATE = 'alpha 2'
 AIL_VERSION_NAME = 'Klee'
 
 AIL_VERSION = '%s.%s%s' % (
@@ -24,6 +24,6 @@ AIL_VERSION_FULL_STRING = '%s.%s%s%s' % (
 AIL_VERSION_NUMBER = 727
 
 
-AIL_COPYRIGHT = '2021 Chenhongbo'
+AIL_COPYRIGHT = '2022 Chenhongbo'
 
 AIL_INSTALL_TIME = -1
