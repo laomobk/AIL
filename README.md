f196fe85e27d219e985f708d81337f12fcffa1d9
Laomo <5491350+LaomoBK@user.noreply.gitee.com>
2022-06-11 21:32:45 +0800

    **version 3.0**

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # AIL 2.3 alpha 3
 
-[AIL 2.3 Diona 版本更新内容](./docs/whatsnew.md)
-
+[AIL 3.0 Lumine 版本更新内容](./docs/whatsnew.md)
+[AIL 2.3 Diona 版本更新内容](./docs/versions/2_3.md)
 [AIL 2.2 Klee 版本更新内容](./docs/versions/2_2.md)
 
 ![AIL badge](https://img.shields.io/badge/AIL-Programming%20Language-blue)
--- ail/core/version.py
+++ ail/core/version.py
@@ -4,17 +4,18 @@
 #
 # 2.1   Leopard     726     2021.5 - 2022.1
 # 2.2   Klee        727     2022.1 - 2022.3
-# 2.3   Diona       728     2022.3 - now
+# 2.3   Diona       728     2022.3 - 2022.6
+# 3.0   Lumine      729     2022.6 - now
 #
 
 from os.path import exists, join
 from .._config import CORE_PATH
 
 
-AIL_MAIN_VERSION = 2
-AIL_SUB_VERSION = [3]
-AIL_VERSION_STATE = 'alpha 3'
-AIL_VERSION_NAME = 'Diona'
+AIL_MAIN_VERSION = 3
+AIL_SUB_VERSION = [0]
+AIL_VERSION_STATE = 'alpha 0'
+AIL_VERSION_NAME = 'Lumine'
 
 AIL_VERSION = '%s.%s %s' % (
     AIL_MAIN_VERSION,
@@ -30,7 +31,7 @@ AIL_VERSION_FULL_STRING = '%s.%s%s%s' % (
 
 )
 
-AIL_VERSION_NUMBER = 728
+AIL_VERSION_NUMBER = 729
 
 
 AIL_COPYRIGHT = '2022 Chenhongbo'
